34fbc301ac6cc4259e3eb292f19c80c45e48e33c
tanacsg <tanacsg@gmail.com>
2020-12-20 01:48:55 +0100

    serve Angular application from express.js

--- server/serverNode.js
+++ server/serverNode.js
@@ -1,19 +1,27 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const uuid = require('uuid/v1');
 const rp = require('request-promise');
+const { static } = require('express');
 
 const app = express();
+
 const port = process.argv[2];
+const staticRoot = __dirname + '/dist/';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', function(req, res) {
 
-app.get('/', function (req, res) {
-  res.send("Blockchain Poll");
+	console.log(__dirname)
+	console.log(staticRoot)
+	res.sendFile('./dist/index.html', { root: __dirname });
 });
 
+app.use(express.static(staticRoot));
 
 app.listen(port, function() {
 	console.log(`Listening on port ${port}...`);
