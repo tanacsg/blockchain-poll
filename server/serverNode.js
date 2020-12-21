809760177e7918471e51991ef1d3c1cce2387da8
tanacsg <tanacsg@gmail.com>
2020-12-21 01:35:21 +0100

    connect to MongoDB

--- server/serverNode.js
+++ server/serverNode.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongo = require('mongodb');
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const uuid = require('uuid/v1');
 const rp = require('request-promise');
 const { static } = require('express');
+
+const MongoClient = mongo.MongoClient;
+const url = "mongodb://root:example@localhost:27017/mydb?authSource=admin";
 
 const app = express();
 
@@ -20,6 +24,22 @@ app.get('/', function(req, res) {
 	console.log(staticRoot)
 	res.sendFile('./dist/index.html', { root: __dirname });
 });
+
+app.get('/mongodb', function(req, res) {
+
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		const dbo = db.db("mydb");
+			dbo.createCollection("polls", function(err, res) {
+				if (err) throw err;
+				console.log("Collection created!");
+				db.close();
+			  });	
+		  });
+
+	res.send('Mongo DB');
+});
+
 
 app.use(express.static(staticRoot));
 
