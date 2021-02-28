ce0faff9df802985cbeca7f954c67df41b208184
tanacsg <tanacsg@gmail.com>
2021-03-01 00:33:30 +0100

    improve poll create form

--- client/src/app/poll-edit/poll-edit.component.ts
+++ client/src/app/poll-edit/poll-edit.component.ts
@@ -43,9 +43,7 @@ export class PollEditComponent implements OnInit {
       this.serverMessage = o.message;
       this.confirmedPollId = o.id
       this.registerUrl = this.baseUrl + "register/" + this.confirmedPollId
-      console.log("Register URL: " + this.registerUrl)
       this.voteUrl = this.baseUrl + "poll/" + this.confirmedPollId
-      console.log("Vote URL: " + this.voteUrl)
 
     }, err => {
       this.serverErrorMessage = err.error.message;
